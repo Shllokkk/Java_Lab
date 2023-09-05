cb7dd27ba400aa4ecfe6b527793d5109b634af03
Shllokkk <shllokosan23@gmail.com>
2023-09-06 00:14:53 +0530

    adding on 06/09/23

--- Problem1.java
+++ Problem1.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Employee{
 	int empno;
 	int basic;
-	String empname=new String();
+	String empname;
 	
 	Employee(){
 		Scanner sc= new Scanner(System.in);
--- Problem2.java
+++ Problem2.java
@@ -4,19 +4,26 @@ class Racer{
 	int speed;
 	Racer(int i){
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter Speed  of Biker"+i+": ");
+		System.out.print("Enter Speed  of Biker "+(i+1)+": ");
 		speed=sc.nextInt();
 	}
 }
 
 class Problem2{
 	public static void main(String[] args){
-		int sum;
+		int sum=0;
 		Racer[] r=new Racer[5];
 		for(int i=0;i<r.length;i++){
 			r[i]=new Racer(i);
-			sum+=r[i].speed;
+			sum+=+r[i].speed;
 		}
-		
+
+		System.out.println("The average speed is: "+sum/5);
+
+		for(int i=0;i<r.length;i++){
+			if(r[i].speed>=sum/5){
+				System.out.println("Biker "+i+" has qualified!");
+			}
+		}
 	}
 }
